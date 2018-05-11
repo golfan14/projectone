1dd9ebd08aab310eca881df1828c7198be0a3a74
Kennan Smith <kennan.smith1@gmail.com>
2018-05-11 10:45:58 -0600

    added firebase initialization

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+// INITIALIZE FIREBASE AND STORE THE DATA IN VARIABLE 'database'
+var config = {
+    apiKey: "AIzaSyBw-1_TEm8sv798vjUwPtDoN3TJMP1qQGs",
+    authDomain: "group-project-1-f306f.firebaseapp.com",
+    databaseURL: "https://group-project-1-f306f.firebaseio.com",
+    projectId: "group-project-1-f306f",
+    storageBucket: "",
+    messagingSenderId: "268163003813"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
     $("body").on("click", "#landingButton", function(event){
     event.preventDefault();
     $(".mainContainer").empty()
@@ -20,12 +36,16 @@ $(document).ready(function() {
     $("#mainSection").append("<form class='form' id='mainForm'>")
     $("#mainForm").append("<row class='formRow'>")
 //User input field for SOMETHING
-    $(".formRow").append("<input class='formInput' id='SOMETHING' type='text' maxlength='30'>");
     $(".formRow").append("<label id='tagSOMETHING' for='SOMETHING'>");
+    $(".formRow").append("<input class='formInput' id='SOMETHING' type='text' maxlength='30'>");
     $("#tagSOMETHING").text("SOMETHING");
 //User input field for SOMETHING ELSE
+    $(".formRow").append("<label id='tagSOMETHINGELSE' for='SOMETHINGELSE'>");  
     $(".formRow").append("<input class='formInput' id='SOMETHINGELSE' type='text' maxlength='30'>");
-    $(".formRow").append("<label id='tagSOMETHINGELSE' for='SOMETHINGELSE'>");
+    $("#tagSOMETHINGELSE").text("SOMETHINGELSE");
+//User input field for SOMETHING ELSE
+    $(".formRow").append("<label id='tagSOMETHINGELSE' for='SOMETHINGELSE'>");  
+    $(".formRow").append("<input class='formInput' id='SOMETHINGELSE' type='text' maxlength='30'>");
     $("#tagSOMETHINGELSE").text("SOMETHINGELSE");
 //Submit button for our input fields
     $(".formRow").append("<button class='button' id='mainButton' type='submit'>")
